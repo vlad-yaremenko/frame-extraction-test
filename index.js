d81d62410a0c6ab3a7f67d8801be3518b0070ab6
Vladyslav Yaremenko <vladyaremenko95.vy@gmail.com>
2025-02-16 18:23:34 +0200

    Update

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 const path = require("path");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const VIDEO_PATH = path.join(__dirname, "video.mp4"); // Change to your 4K video file
 
 app.use(express.static(path.join(__dirname, "public")));
